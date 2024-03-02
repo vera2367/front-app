d65918e1e0445345db8a3439ccb9c8a789a83236
vera2367 <oscarestebanvera00@hotmail.com>
2024-03-02 17:47:50 -0500

    add clients and directives

---
+++ src/app/client/clients.json.ts
@@ -0,0 +1,14 @@
+import {Cliente} from "./cliente";
+export const CLIENTES: Cliente[] = [
+  {id:1,nombre:'Esteban',apellido:'Vera',createAt:'2017-12-11',email:'oscaresteban2000@gmail.com'},
+  {id: 2,nombre: "Shem",apellido: "Hawker",email: "shawker1@netscape.com",createAt: "2/14/2024"},
+  {id: 3,nombre: "Reinaldos",apellido: "Do",email: "rdo2@ebay.com",createAt: "10/3/2023"},
+  {id: 4,nombre: "Stavros",apellido: "Jagielski",email: "sjagielski3@cbc.ca",createAt: "7/6/2023"},
+  {id: 5,nombre: "Inna",apellido: "Ticehurst",email: "iticehurst4@nationalgeographic.com",createAt: "2/28/2024"},
+  {id: 6,nombre: "Trumann",apellido: "Coulthart",email: "tcoulthart5@sina.com.cn",createAt: "9/16/2023"},
+  {id: 7,nombre: "Willem",apellido: "Lempke",email: "wlempke6@ehow.com",createAt: "6/17/2023"},
+  {id: 8,nombre: "Gregg",apellido: "Poone",email: "gpoone7@ca.gov",createAt: "10/1/2023"},
+  {id: 9,nombre: "Sindee",apellido: "Wintringham",email: "swintringham8@thetimes.co.uk",createAt: "10/12/2023"},
+  {id: 10,nombre: "Marianne",apellido: "Burthom",email: "mburthom9@blogtalkradio.com",createAt: "10/2/2023"},
+  {id: 11,nombre: "Carlie",apellido: "Segrott",email: "csegrotta@amazonaws.com",createAt: "2/14/2024"},
+];
