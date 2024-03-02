1902cecd5dde99e631b3902b777205f0e22de700
vera2367 <oscarestebanvera00@hotmail.com>
2024-03-02 17:47:50 -0500

    correct routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
+import {DirectivaComponent} from "./directiva/directiva.component";
+import {ClientComponent} from "./client/client.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, DirectivaComponent, ClientComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {ClientComponent} from "./client/client.component";
+import {DirectivaComponent} from "./directiva/directiva.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',redirectTo:'/clients', pathMatch:'full'},
+  {path:'clientes',component:ClientComponent},
+  {path:'directivas',component:DirectivaComponent},
+];
